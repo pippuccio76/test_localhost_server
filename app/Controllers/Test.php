cc868a42a2de59366a637db98ff5f8e4e1444825
stefanobanchelli <stefanobanchelli@yahoo.it>
2022-02-16 19:29:16 +0100

    Init

---
+++ app/Controllers/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class Test extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+
+    public function test()
+    {
+        echo 'test';
+    }
+}
